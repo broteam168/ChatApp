50390bcfd271bc743e7bccb99a6668f4764b31bf
Nguyễn Hoàng Sơn <broteam168@gmail.com>
2023-09-21 19:11:24 +0700

    Add project files.

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ChatApp.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Android.Content.ClipData;
+
+namespace ChatApp.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+        bool isBusy = false;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return this.isBusy;
+            }
+            set
+            {
+                this.isBusy = value;
+            }
+        }
+        protected bool setProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "", 
+            Action onChanged = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(backingStore, value))
+            {
+                return false;
+            }
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
